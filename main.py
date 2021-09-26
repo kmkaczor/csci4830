f24524b72a67a0ddb131e8ba5cd891bf8ec14505
Korey Kaczor <koreykaczor@unomaha.edu>
2021-09-25 22:11:37 -0500

    Basic skeleton program without binaries.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+class Author(models.Model):
+
 class Book(models.Model):
-    author_lastname = models.CharField(max_length=30)
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE) # We use a foreign key as Author is its own table
     author_firstname = models.CharField(max_length=30)
