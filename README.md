79c174d3b4dff9fdcdf0389ca472eb7dfb5d5f30
Korey Kaczor <koreykaczor@unomaha.edu>
2021-09-26 11:15:00 -0500

    Added django files

---
+++ README.md
@@ -0,0 +1,7 @@
+This is the root folder for the Semester Project of the CSCI4830 Intro to Software Engineering.
+
+The current developers are:
+    * Korey Kaczor
+    *
+    *
+    *
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# main.py
+# Korey Kaczor
+# CSCI 4830 Hello World
+
+
 from django.db import models
 from django.db.models.fields import CharField, IntegerField
 
@@ -11,4 +16,4 @@ class Book(models.Model):
     # We use a foreign key as Author is its own table
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     title = CharField(max_length=250)
-    isbn = IntegerFiel(13)  # Might change this to char
+    isbn = IntegerField(13)  # Might change this to char
