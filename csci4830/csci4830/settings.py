72d29d2121213d4c3add78bdfa8f60defaaaae1d
Korey Kaczor <kmkaczor@unomaha.edu>
2021-11-17 19:37:59 -0600

    Move to SQLlite for windows dev environments

--- csci4830/csci4830/settings.py
+++ csci4830/csci4830/settings.py
@@ -107,23 +107,23 @@ WSGI_APPLICATION = 'csci4830.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'CSCI4830project',
+#        'USER': 'CSCI4830',
+#        'PASSWORD': 'CSCI4830Django',
+#        'HOST': AWS_DEVEL_DB,  # "127.0.0.1",
+#        'PORT': '3306',
+#    },
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'CSCI4830project',
-        'USER': 'CSCI4830',
-        'PASSWORD': 'CSCI4830Django',
-        'HOST': AWS_DEVEL_DB,  # "127.0.0.1",
-        'PORT': '3306',
-    },
-}
-
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+ }
 
 
 # Password validation
